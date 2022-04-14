ef4fab81697660e49350d7e8043a3c558d2e79c8
heekng <rhkd1769@gmail.com>
2022-04-15 02:55:41 +0900

    Member Entity 작성

--- src/main/java/com/heekng/springboot_oauth_test/entity/Member.java
+++ src/main/java/com/heekng/springboot_oauth_test/entity/Member.java
@@ -1,15 +1,47 @@
 package com.heekng.springboot_oauth_test.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
-    @Id
-    private String id;
+    @Id @GeneratedValue
+    private Long id;
+
+    private String email;
+    private String name;
     private String password;
+    private String picture;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public Member(String email, String name, String password, Role role, String picture) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.role = role;
+        this.picture = picture;
+    }
+
+    public Member update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRole() {
+        return this.role.toString();
+    }
+
+
+
 }
