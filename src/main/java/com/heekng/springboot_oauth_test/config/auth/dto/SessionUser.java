49ec5329c0bab87c60a8c1d77d3edfb5b7e89d27
heekng <rhkd1769@gmail.com>
2022-04-15 02:56:25 +0900

    security OAuth 설정 추가

---
+++ src/main/java/com/heekng/springboot_oauth_test/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.heekng.springboot_oauth_test.config.auth.dto;
+
+import com.heekng.springboot_oauth_test.entity.Member;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(Member member) {
+        this.name = member.getName();
+        this.email = member.getEmail();
+        this.picture = member.getPicture();
+    }
+}
